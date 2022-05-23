05ad3c1519a437307b731cef19b6fc0bc18562df
JeffLi <lidfdsffd49@gmail.com>
2022-05-24 00:41:24 +0800

    quick sort finish

--- Jeff/Sorting/quick_sort.c
+++ Jeff/Sorting/quick_sort.c
@@ -1,8 +1,57 @@
 #include <stdio.h>
+#define list_size 20
 
-int main() {
-    int a;
-    scanf("%d", &a);
-    printf("hello world + %d", a);
-    return 0;
+void loop_list(int list[], int size);
+void quick_sort(int list[], int size);
+void swap(int* a, int* b);
+
+int main(){
+    int list[list_size];
+    for (int i=0; i<list_size; i++) {
+        list[i] = ((((i+47)*79-31)/19)*747) % 17;
+    }
+    printf("Unsort:\t");
+    loop_list(list, list_size);
+    quick_sort(list, list_size);
+    printf("Sort:\t");
+    loop_list(list, list_size);
+}
+
+void loop_list(int list[], int size){
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", list[i]);
+    }
+    printf("\n");
+}
+
+void quick_sort_loop(int list[], int low, int high);
+
+void quick_sort(int list[], int size){
+    quick_sort_loop(list, 0, size);
+}
+
+void quick_sort_loop(int list[], int low, int high) {
+    int pivot = low;    // middle element
+    int i = high;   // checking element
+    int j = high;   // bigger element
+    if (low >= high) {
+        return;
+    }
+    while (i > pivot) {
+        if (list[i] > list[pivot]) {
+            swap(&list[i], &list[j]);
+            j--;
+        }
+        i--;
+    }
+    swap(&list[pivot], &list[j]);
+    quick_sort_loop(list, low, j-1);
+    quick_sort_loop(list, j+1, high);
+}
+
+void swap(int* a, int* b) {
+    int temp = *b;
+    *b = *a;
+    *a = temp;
 }
