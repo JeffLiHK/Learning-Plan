aa6013788becbdfa1a95d4fb1d38a91151dfc64d
JeffLi <lidfdsffd49@gmail.com>
2022-06-03 01:28:40 +0800

    exer-4-13

---
+++ Jeff/Exercise/4-13-reverse.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void reverse(char* rev, char* str, int len, int i);
+
+int main() {
+    char str[] = "hello world";
+    char new[12];
+    reverse(new, str, 11, 0);
+    printf("\n%s\n", new);
+    return 0;
+}
+
+void reverse(char* rev, char* str, int len, int i) {
+    rev[i++] = str[len-i-1];
+    if (i < len) {
+        reverse(rev, str, len, i);
+    } else {
+        rev[i] = '\0';
+    }
+}
